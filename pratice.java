4b9eaad4e6d2cb773911de224ca4f15360179256
Tejasai-S <tjg41699@gmail.com>
2025-02-03 22:43:29 +0530

    3/2/2025 10:45PM

---
+++ pratice.java
@@ -0,0 +1,25 @@
+//simple print statement of hello world
+/*class pratice{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}*/
+//simple program using method
+/*class box{
+    public static void main(String args[]){
+        box1 ob = new box1();
+        ob.height=10;
+        ob.width=20;
+        ob.depth=30;
+        ob.volume();
+
+    }
+}
+class box1{
+    double height;
+    double width;
+    double depth;
+    void volume(){
+        System.out.println("volume: "+height*width*depth);
+    }
+}*/
