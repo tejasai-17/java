423e609dbc1470b90b9409978b9afdc52ebb9b8f
Tejasai-S <tjg41699@gmail.com>
2025-02-12 22:49:15 +0530

    12/02/2025 10:30

---
+++ if_elseif_else.java
@@ -0,0 +1,13 @@
+public class if_elseif_else {
+    public static void main(String[] args) {
+        int n = 10;
+
+        if (n > 0) {
+            System.out.println("The number is positive.");
+        } else if (n < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
